d2d506db3315a5e9492d2b3784c846d6b15b9c9e
supersha <supersha@foxmail.com>
2013-06-01 14:53:22 +0800

    Update login.js

--- app/page/login.js
+++ app/page/login.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 exports.showPage = function(req,res){
 
 	// 检测Safy是否已经初始化
-	fs.exists("./lib/setting.js",function(exists){
+	fs.exists("./app/setting.js",function(exists){
 		if(exists){
 			res.render("login.html",{page:"login"});
 		}else{
